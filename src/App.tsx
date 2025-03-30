c0a0600b39679f1ea95a35659b14c8fe73408fec
jchyeon0814 <jchyeon0814@gmail.com>
2025-03-30 17:00:56 +0900

    할 일 추가 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import styled from '@emotion/styled';
 import { DataView } from 'components/DataView';
 import { TodoInput } from 'components/TodoInput';
-
+import { Button } from 'components/Button'
 
 const Container = styled.div`
   height: 100vh;
@@ -16,12 +16,22 @@ const Container = styled.div`
   background-color: #eeeeee;
 `;
 
+const ShowInputButton = styled.div`
+  position: absolute;
+  right: 40px;
+  bottom: 40px;
+  z-index: 1;
+`;
+
+
 function App() {
   const [todoList, setTodoList] = useState([
     '리액트 공부하기',
     '운동하기',
     '책 읽기',
   ]);
+
+  const [showTodoInput, setShowTodoInput] = useState(false);
 
   const onDelete = (todo: string) => {
     setTodoList(todoList.filter((item) => item !== todo));
@@ -31,12 +41,20 @@ function App() {
     if (todo === '') return;
 
     setTodoList([...todoList, todo]);
+    setShowTodoInput(false);
   }
 
   return (
     <Container>
       <DataView title="할 일 목록" onDelete={onDelete} todoList={todoList} />
-      <TodoInput onAdd={onAdd} />
+      {showTodoInput && <TodoInput onAdd={onAdd} />}
+      <ShowInputButton>
+        <Button 
+          label={showTodoInput ? '닫기' : '할 일 추가'}
+          color={showTodoInput ? undefined : '#304FFE'} 
+          onClick={() => setShowTodoInput(!showTodoInput)}
+        />
+      </ShowInputButton>
     </Container>
   );
 }
