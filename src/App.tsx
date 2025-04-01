3c6c63b36999b8c3d24ee86b47b50a39c1590cc7
jchyeon0814 <jchyeon0814@gmail.com>
2025-04-02 08:05:31 +0900

    Provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
 import styled from '@emotion/styled';
 import { DataView } from 'components/DataView';
 import { InputContainer } from 'components/InputContainer';
+import { TodoListContextProvider } from 'contexts/TodoList'
 
 const Container = styled.div`
   height: 100vh;
@@ -16,27 +16,13 @@ const Container = styled.div`
 `;
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    '리액트 공부하기',
-    '운동하기',
-    '책 읽기',
-  ]);
-
-
-  const onDelete = (todo: string) => {
-    setTodoList(todoList.filter((item) => item !== todo));
-  };
-
-  const onAdd = (todo: string) => {
-    if (todo === '') return;
-
-    setTodoList([...todoList, todo]);
-  }
-
+  
   return (
     <Container>
-      <DataView title="할 일 목록" onDelete={onDelete} todoList={todoList} />
-      <InputContainer onAdd={onAdd} />
+      <TodoListContextProvider>
+        <DataView title="할 일 목록" />
+        <InputContainer />
+      </TodoListContextProvider>
     </Container>
   );
 }
